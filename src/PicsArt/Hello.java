5739a2d3bfb91c4219475d52f50c5bde39fce64d
MesMik <mesmiq@gmail.com>
2020-10-28 21:17:55 +0400

    Second try

---
+++ src/PicsArt/Hello.java
@@ -0,0 +1,14 @@
+package PicsArt;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Lesson 1");
+        byte x = 8;
+        System.out.println(x);
+        short y = 222;
+        System.out.print(y + " ");
+        int a = 123456789;
+        System.out.println(a);
+    }
+}
---
+++ src/PicsArt/JavaLesson.java
@@ -0,0 +1,4 @@
+package PicsArt;
+
+public class JavaLesson {
+}
